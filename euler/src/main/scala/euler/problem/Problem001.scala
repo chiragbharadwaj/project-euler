5291e69bc41f86ca5205c525c87ad8ab15db2ff7
chiragbharadwaj <cb625@cornell.edu>
2025-07-05 18:50:28 -0400

    feat(problem-1): finished first problem, added some formatting as well

--- euler/src/main/scala/euler/problem/Problem001.scala
+++ euler/src/main/scala/euler/problem/Problem001.scala
@@ -1,7 +1,22 @@
 package euler.problem
 
 object Problem001 {
+  private def isMultipleOfAny(factors: List[Int])(n: Int) =
+    factors
+      .map(n % _ == 0)
+      .reduce(_ || _)
+
+  private def getSumOfMultiples(factors: List[Int], limit: Int) =
+    (1 until limit).view
+      .filter(isMultipleOfAny(factors))
+      .sum
+
   def main(args: Array[String]): Unit = {
-    println("Hello world!")
+    val factors = List(3, 5)
+    val limit = 1000
+
+    val sum = getSumOfMultiples(factors, limit)
+
+    println(s"The sum of all multiples of 3 and 5 below 1000 is: $sum")
   }
 }
